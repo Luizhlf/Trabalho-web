847df2e8ff679f75bfe4bd71ae8cb6e0df83e728
Luizhlf <luiz301202lh@gmail.com>
2025-11-25 15:50:40 -0300

    web scraping

--- app.js
+++ app.js
@@ -15,20 +15,19 @@ function raspar(doc) {
 
 
 function scraping(){
-    fetch("https://proxy.corsfix.com/?https://manhuaus.com/")
-    .then(response => {
-        if(!response.ok){
-            throw new Error("Chamada HTTP Falhou")
-        }
-        return response.text()
-    })
-    .then(dados => {
-        let parser = new DOMParser()
-        let doc = parser.parseFromString(dados, "text/html")
-        raspar(doc)
-    })
-    .catch(erro => alert(erro))
+    fetch("https://api.allorigins.win/raw?url=https://manhuaus.com/")
+        .then(response => {
+            if(!response.ok) throw new Error("Chamada HTTP Falhou")
+            return response.text()
+        })
+        .then(dados => {
+            let parser = new DOMParser()
+            let doc = parser.parseFromString(dados, "text/html")
+            raspar(doc)
+        })
+        .catch(erro => alert(erro))
 }
+
 
 function popularEvento(){
     document.querySelector('#btn').addEventListener("click", evt => {
